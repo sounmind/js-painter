7afdb45e877283a3c17cb865a2427524b60a3322
sounmind <sounmind777@gmail.com>
2021-01-25 20:19:36 +0900

    feat(brush mode & save function)

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const canvas = document.getElementById("jsCanvas");
 const context = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const brushSize = document.getElementById("jsBrushSize");
-const defaultLineWidth = 5;
+const modeButton = document.getElementById("jsModeButton");
+const saveButton = document.getElementById("jsSaveButton");
 
-canvas.width = 700;
-canvas.height = 700;
-context.strokeStyle = "black";
-context.lineWidth = defaultLineWidth;
+const DEFAULT_LINE_WIDTH = 5;
+const DEFAULT_COLOR = "black";
+const CANVAS_DEFAULT_WIDTH = 700;
+const CANVAS_DEFAULT_HEIGHT = 700;
+const DEFAULT_BACKGROUND_COLOR = "white";
 
 let painting = false;
+let filling = false;
+
+canvas.width = CANVAS_DEFAULT_WIDTH;
+canvas.height = CANVAS_DEFAULT_HEIGHT;
+
+// 배경 설정
+context.fillStyle = DEFAULT_BACKGROUND_COLOR;
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+// 브러시 색깔, 크기 초기화
+context.strokeStyle = DEFAULT_COLOR;
+context.lineWidth = DEFAULT_LINE_WIDTH;
 
 function startPainting() {
   painting = true;
@@ -40,6 +54,7 @@ function onMouseDown(event) {
 function handleColorClick(event) {
   const colorClicked = event.target.style.backgroundColor;
   context.strokeStyle = colorClicked;
+  context.fillStyle = colorClicked;
 }
 
 function handleBrushSizeChange(event) {
@@ -47,11 +62,41 @@ function handleBrushSizeChange(event) {
   context.lineWidth = brushSizeChanged;
 }
 
+function handleModeChange(event) {
+  if (filling) {
+    filling = false;
+    modeButton.innerText = "Mode: Fill";
+  } else {
+    filling = true;
+    modeButton.innerText = "Mode: Paint";
+  }
+}
+
+function handleCanvasClick(event) {
+  if (filling) {
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+function handleContextMenu(event) {
+  event.preventDefault();
+}
+
+function handleSaveButtonClick(event) {
+  const canvasImage = canvas.toDataURL();
+  const downloadLink = document.createElement("a");
+  downloadLink.href = canvasImage;
+  downloadLink.download = "exportImage"; // 저장할 때 파일 이름
+  downloadLink.click();
+}
+
 if (canvas.getContext) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleContextMenu);
 } else {
   console.log("This Browser does not support Canvas!");
 }
@@ -62,4 +107,12 @@ Array.from(colors).forEach((color) =>
 
 if (brushSize) {
   brushSize.addEventListener("input", handleBrushSizeChange);
+}
+
+if (modeButton) {
+  modeButton.addEventListener("click", handleModeChange);
+}
+
+if (saveButton) {
+  saveButton.addEventListener("click", handleSaveButtonClick);
 }
